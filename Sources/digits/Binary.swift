e28c4a5b25b58888c184a4c820cb6f4c0e778cf9
Korben Rusek <korben.rusek@gmail.com>
2019-09-06 18:19:33 -0700

    remove unneeded "Number" enum
    Now BinaryInteger can be positive or negative so "Number" and "Binary" are not needed.

--- Sources/digits/Binary.swift
+++ Sources/digits/Binary.swift
@@ -1,5 +1,3 @@
-public typealias Binary = List<Digit, Void>
-
 public indirect enum BinaryInteger {
     case list(Digit, BinaryInteger), empty(Sign)
 }
@@ -43,39 +41,6 @@ extension BinaryInteger: CustomStringConvertible {
         return r
     }
 
-    private func digit(radix: Int) -> String {
-        for ix in 0..<10 {
-            if self == .build(ix) {
-                return String(ix, radix: radix)
-            }
-        }
-        return ""
-    }
-}
-
-extension List: CustomStringConvertible where Element == Digit, Empty == Void {
-    public var description: String {
-        return base10()
-    }
-    
-    func base10() -> String {
-        return toString(radix: 10)
-    }
-    
-    public func toString(radix: Int) -> String {
-        guard self != .zero else { return "0" }
-        
-        let rb = Binary.build(radix)
-        var value = self
-        var remainder = Binary.zero
-        var r = ""
-        while value != .zero {
-            (value, remainder) = try! Binary.integerDivide(divisor: rb, dividend: value)
-            r = remainder.digit(radix: radix) + r
-        }
-        return r
-    }
-    
     private func digit(radix: Int) -> String {
         for ix in 0..<10 {
             if self == .build(ix) {
@@ -205,163 +170,8 @@ extension BinaryInteger {
     }
 }
 
-extension List where Element == Digit, Empty == Void {
-    public static var zero: Binary {
-        return .list(.zero, .empty(()))
-    }
-    
-    public static var one: Binary {
-        return .list(.one, .empty(()))
-    }
-    
-    public func incremented() -> Binary {
-        switch self {
-        case .list(.zero, let list):
-            return .list(.one, list)
-        case .list(.one, let list):
-            return .list(.zero, list.incremented())
-        default:
-            return .list(.one, self)
-        }
-    }
-    
-    public static func build(_ value: Int) -> Binary {
-        var b = Binary.empty(())
-        var v = value
-        while v > 0 {
-            if v % 2 == 1 {
-                b = .list(.one, b)
-                v -= 1
-                v = v / 2
-            } else {
-                b = .list(.zero, b)
-                v = v / 2
-            }
-        }
-        let r = b.reversed()
-        return r
-    }
-    
-    func shifted() -> Binary {
-        switch self {
-        case .empty:
-            return .empty(())
-        default:
-            return .list(.zero, self)
-        }
-    }
-    
-    public func mod5(_ index: Int = 0, _ accumulator: Binary = .zero) -> Binary {
-        let powers: [Binary] = [
-            .build(1), // 1 % 5 = 1
-            .build(2), // 2 % 5 = 2
-            .build(4), // 4 % 5 = 4
-            .build(3)  // 8 % 5 = 3
-        ]
-        if self == .build(5) && index == 0 && accumulator == .zero { return .build(0) }
-        if self == .build(6) && index == 0 && accumulator == .zero { return .build(1) }
-        if self == .build(7) && index == 0 && accumulator == .zero { return .build(2) }
-        if self == .build(8) && index == 0 && accumulator == .zero { return .build(3) }
-        if self == .build(9) && index == 0 && accumulator == .zero { return .build(4) }
-        if self == .build(10) && index == 0 && accumulator == .zero { return .build(0) }
-        switch self {
-        case .empty where accumulator == Binary.build(5):
-            return .zero
-        case .empty where accumulator > Binary.build(4):
-            return accumulator.mod5()
-        case .empty:
-            return accumulator
-        case .list(let d, let tail):
-            return tail.mod5((index + 1) % 4, accumulator + d * powers[index % 4])
-        }
-    }
-    
-    fileprivate static func subtract(lhs: Binary, rhs: Binary, carrying: Bool = false) -> Binary {
-        switch (lhs, rhs) {
-        case (.list(.one, let tail1), .list(.one, let tail2)) where carrying,
-             (.list(.zero, let tail1), .list(.zero, let tail2)) where carrying:
-            return .list(.one, subtract(lhs: tail1, rhs: tail2, carrying: true))
-        case (.list(.one, let tail1), .list(.one, let tail2)),
-             (.list(.zero, let tail1), .list(.zero, let tail2)),
-             (.list(.one, let tail1), .list(.zero, let tail2)) where carrying:
-            return .list(.zero, subtract(lhs: tail1, rhs: tail2, carrying: false))
-        case (.list(.one, let tail1), .list(.zero, let tail2)):
-            return .list(.one, subtract(lhs: tail1, rhs: tail2, carrying: false))
-        case (.list(.one, let tail), .empty) where carrying:
-            return .list(.zero, tail)
-        case (.list(.zero, let tail1), .list(.one, let tail2)):
-            return .list(carrying ? .zero : .one, subtract(lhs: tail1, rhs: tail2, carrying: true))
-        case (.list(.zero, let tail), .empty) where carrying:
-            return .list(.one, subtract(lhs: tail, rhs: .empty(()), carrying: true))
-        case (.list(.zero, _), .empty),
-             (.list(.one, _), .empty),
-             (.empty, _):
-            return lhs
-        }
-    }
-    
-    fileprivate static func reduce<Result>(lhs: Binary, rhs: Binary, initial: Result, f: (Digit, Digit, Result) -> Result) -> Result {
-        switch (lhs, rhs) {
-        case (.empty, .empty):
-            return f(.zero, .zero, initial)
-        case (.empty, .list(let d, let tail)):
-            return reduce(lhs:.empty(()), rhs:tail, initial:f(.zero, d, initial), f:f)
-        case (.list(let d, let tail), .empty):
-            return reduce(lhs:tail, rhs:.empty(()), initial:f(d, .zero, initial), f:f)
-        case (.list(let d, let ltail), .list(let e, let rtail)):
-            return reduce(lhs: ltail, rhs: rtail, initial:f(d, e, initial), f:f)
-        }
-    }
-
-    public func pow(_ power: Binary, partial: Binary = .one) -> Binary {
-        switch power {
-        case .empty:
-            return partial
-        case .list(.one, let tail):
-            return (self * self).pow(tail, partial: partial * self)
-        case .list(.zero, let tail):
-            return (self * self).pow(tail, partial: partial)
-        }
-    }
-}
-
-extension List: ExpressibleByIntegerLiteral where Element == Digit, Empty == Void {
-    public init(integerLiteral value: Int) {
-        var b = Binary.empty(())
-        var v = value
-        while v > 0 {
-            if v % 2 == 1 {
-                b = .list(.one, b)
-                v -= 1
-                v = v / 2
-            } else {
-                b = .list(.zero, b)
-                v = v / 2
-            }
-        }
-        let r = b.reversed()
-        self = r
-    }
-    
-    public typealias IntegerLiteralType = Int
-}
-
 public enum ArithmeticError: Error {
     case divideByZero
-}
-
-extension List: AdditiveArithmetic where Element == Digit, Empty == Void {
-    public static func += (lhs: inout Binary, rhs: Binary) {
-        lhs = lhs + rhs
-    }
-    
-    public static func -= (lhs: inout Binary, rhs: Binary) {
-        lhs = lhs - rhs
-    }
-    
-    public static func -(lhs: Binary, rhs: Binary) -> Binary {
-        return Binary.subtract(lhs: lhs, rhs: rhs)
-    }
 }
 
 extension BinaryInteger: Equatable, Comparable {
@@ -430,33 +240,6 @@ extension BinaryInteger: Equatable, Comparable {
             return reduce(lhs:tail, rhs:rhs, initial:f(d, sign.digit, initial), f:f)
         case (.list(let d, let ltail), .list(let e, let rtail)):
             return reduce(lhs: ltail, rhs: rtail, initial:f(d, e, initial), f:f)
-        }
-    }
-}
-
-extension List: Comparable, Equatable where Element == Digit, Empty == Void {
-    public static func ==(lhs: Binary, rhs: Binary) -> Bool {
-        switch (lhs, rhs) {
-        case (.empty, .empty):
-            return true
-        case (.list(.zero, let rtail), .list(.zero, let ltail)),
-             (.list(.one, let rtail), .list(.one, let ltail)):
-            return rtail == ltail
-        case (.list(.zero, let tail), .empty),
-             (.empty, .list(.zero, let tail)):
-            return tail == .empty(())
-        default:
-            return false
-        }
-    }
-    
-    public static func <(lhs: Binary, rhs: Binary) -> Bool {
-        return Binary.reduce(lhs: lhs, rhs: rhs, initial: false) { (a, b, r) -> Bool in
-            if a == b {
-                return r
-            } else {
-                return a == .zero
-            }
         }
     }
 }
@@ -632,68 +415,5 @@ extension BinaryInteger {
         case .list(.zero, let tail):
             return (self * self).pow(tail, partial: partial)
         }
-    }
-}
-
-extension List where Element == Digit, Empty == Void {
-    public static func +(lhs: Binary, rhs: Binary) -> Binary {
-        switch (lhs, rhs) {
-        case (.empty, let tail),
-             (let tail, .empty(())):
-            return tail
-        case (.list(let e, let tail1), .list(.zero, let tail2)),
-             (.list(.zero, let tail1), .list(let e, let tail2)):
-            return .list(e, tail1 + tail2)
-        case (.list(.one, let tail1), .list(.one, let tail2)):
-            return .list(.zero, (tail1 + tail2).incremented())
-        }
-    }
-
-    public static func *(e: Digit, n: Binary) -> Binary {
-        switch e {
-        case .one:
-            return n
-        case .zero:
-            return .zero
-        }
-    }
-
-    public static func *(lhs: Binary, rhs: Binary) -> Binary {
-        switch (lhs, rhs) {
-        case (.empty, _), (_, .empty):
-            return .empty(())
-        case (.list(let d, let tail), let rhs):
-            return d * rhs + tail * rhs.shifted()
-        }
-    }
-    
-    private static func partialDivide(divisor: Binary, dividend: Binary) throws -> (quotient: Binary, remainder: Binary) {
-        guard divisor != .zero else { throw ArithmeticError.divideByZero }
-        if divisor > dividend { return (quotient: .zero, remainder: dividend) }
-        if divisor == dividend { return (quotient: .one, remainder: .zero) }
-        
-        var product = divisor
-        var power = Binary.one
-        while Digit.zero + product <= dividend {
-            product = Digit.zero + product
-            power = Digit.zero + power
-        }
-        return (quotient: power, remainder: dividend - product)
-    }
-    
-    public static func integerDivide(divisor: Binary, dividend: Binary) throws -> (quotient: Binary, remainder: Binary) {
-        if divisor == .zero { throw ArithmeticError.divideByZero }
-        
-        var quotient = Binary.zero
-        var remainder = dividend
-        
-        var (power, r) = try partialDivide(divisor: divisor, dividend: remainder)
-        while power != .zero {
-            quotient += power
-            remainder = r
-            (power, r) = try partialDivide(divisor: divisor, dividend: remainder)
-        }
-        
-        return (quotient: quotient, remainder: remainder)
     }
 }
--- Tests/digitsTests/BinaryTests.swift
+++ Tests/digitsTests/BinaryTests.swift
@@ -2,54 +2,6 @@ import XCTest
 import digits
 
 final class BinaryTests: XCTestCase {
-    
-    func testIncrement() {
-        var b = Binary.zero
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.zero, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.one, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.zero, .zero, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.one, .zero, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.zero, .one, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.one, .one, .one))
-        b = b.incremented()
-        XCTAssertEqual(b, Binary.build(.zero, .zero, .zero, .one))
-    }
-    
-    func testReversedExact() {
-        let b = Binary.build(.one, .zero, .zero, .one, .zero, .one)
-        let reversed = Binary.build(.one, .zero, .one, .zero, .zero, .one)
-        XCTAssertEqual(b.reversed(), reversed)
-    }
-    
-    func testReversedExtraZeros() {
-        let b = Binary.build(.one, .zero, .zero, .one, .zero, .one)
-        let bPadded = Binary.build(.zero, .zero, .zero, .one, .zero, .zero, .one, .zero, .one)
-        
-        let reversed = Binary.build(.one, .zero, .one, .zero, .zero, .one)
-        let reversedPadded = Binary.build(.one, .zero, .one, .zero, .zero, .one, .zero, .zero, .zero)
-        
-        XCTAssertEqual(bPadded.reversed(), reversed)
-        XCTAssertEqual(b.reversed(), reversedPadded)
-        XCTAssertEqual(bPadded.reversed(), reversedPadded)
-    }
-
-    func testOldBuildInteger() {
-        var b = Binary.zero
-        for ix in 0...100 {
-            XCTAssertEqual(b, Binary.build(ix), "bad build for: \(ix)")
-            XCTAssertEqual(b.description, Binary.build(ix).description, "bad description for: \(ix)")
-            b = b.incremented()
-        }
-    }
-
     func testBuildInteger() {
         var p: BinaryInteger = .zero
         var n: BinaryInteger = .zero
@@ -63,15 +15,6 @@ final class BinaryTests: XCTestCase {
         }
     }
 
-    func testOldAddition() {
-        for ix in 0...20 {
-            for iy in 0...20 {
-                let sum = Binary.build(ix) + Binary.build(iy)
-                XCTAssertEqual(sum, Binary.build(ix + iy), "\(ix) + \(iy) != \(sum)")
-            }
-        }
-    }
-
     func testAddition() {
         let limit = 20
         for ix in -limit...limit {
@@ -82,33 +25,12 @@ final class BinaryTests: XCTestCase {
         }
     }
 
-    func testOldMultiplication() {
-        for ix in 0...20 {
-            for iy in 0...20 {
-                let product = Binary.build(ix) * Binary.build(iy)
-                XCTAssertEqual(product, Binary.build(ix * iy), "\(ix) * \(iy) != \(product)")
-            }
-        }
-    }
-
     func testMultiplication() {
         let max = 20
         for ix in -max...max {
             for iy in -max...max {
                 let product = BinaryInteger.build(ix) * BinaryInteger.build(iy)
                 XCTAssertEqual(product, BinaryInteger.build(ix * iy), "\(ix) * \(iy) != \(product)")
-            }
-        }
-    }
-
-    func testOldInequality() {
-        let max = 30
-        for ix in 0...max {
-            for iy in (ix+1)...(max + 1) {
-                let x = Binary.build(ix)
-                let y = Binary.build(iy)
-                XCTAssertTrue(x < y)
-                XCTAssertFalse(y < x)
             }
         }
     }
@@ -124,34 +46,12 @@ final class BinaryTests: XCTestCase {
             }
         }
     }
-    
-    func testMod5() {
-        for ix in 0...1000 {
-            XCTAssertEqual(Binary.build(ix).mod5(), Binary.build(ix % 5), "\(ix) % 5 != \(ix % 5)")
-        }
-    }
-
-    func testOldBase10() {
-        for ix in 0...1000 {
-            XCTAssertEqual(Binary.build(ix).description, "\(ix)", "\(ix) != \(ix)")
-        }
-
-    }
 
     func testBase10() {
         for ix in -1000...1000 {
             XCTAssertEqual(BinaryInteger.build(ix).description, "\(ix)", "\(ix) != \(ix)")
         }
 
-    }
-
-    func testOldSubtraction() {
-        let max = 50
-        for ix in 0...max {
-            for iy in (ix + 1)...(max + 1) {
-                XCTAssertEqual(Binary.build(iy) - Binary.build(ix), Binary.build(iy - ix), "\(iy) - \(ix) != \(iy-ix)")
-            }
-        }
     }
 
     func testSubtraction() {
@@ -161,19 +61,6 @@ final class BinaryTests: XCTestCase {
                 XCTAssertEqual(BinaryInteger.build(iy) - BinaryInteger.build(ix), BinaryInteger.build(iy - ix), "\(iy) - \(ix) != \(iy-ix)")
             }
         }
-    }
-
-    func testOldPower() {
-        let max = 20
-        for ix in 1...6 {
-            for iy in 0...max {
-                let power: Int = Int(pow(Double(ix), Double(iy)))
-                XCTAssertEqual(Binary.build(power), Binary.build(ix).pow(Binary.build(iy)), "\(ix)^\(iy) != \(power)")
-            }
-        }
-
-        let power = Binary.build(2).pow(Binary.build(135))
-        XCTAssertEqual(power.description, "43556142965880123323311949751266331066368")
     }
 
     func testPower() {
@@ -187,17 +74,6 @@ final class BinaryTests: XCTestCase {
 
         let power = BinaryInteger.build(2).pow(BinaryInteger.build(135))
         XCTAssertEqual(power.description, "43556142965880123323311949751266331066368")
-    }
-
-    func testOldDividing() throws {
-        let max = 50
-        for ix in 1...max {
-            for iy in (ix + 1)...(max+1) {
-                let (q, r) = try! Binary.integerDivide(divisor: .build(ix), dividend: .build(iy))
-                XCTAssertEqual(q, Binary.build(iy / ix))
-                XCTAssertEqual(r, Binary.build(iy % ix))
-            }
-        }
     }
 
     func testIntegerDividing() throws {
